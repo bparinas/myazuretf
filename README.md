9d5b5d49adf9611e2fff8dd2d5453efb38cd00dc
bparinas <bparinas@hotmail.com>
2024-05-21 17:27:54 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# myazuretf
+# Get started
+```
+execute in azure cloud shell:
+az ad sp create-for-rbac --name sp_dev101 --role Contributor --scopes /subscriptions/<subs-id>
+{
+  "appId": "sp-clientid",
+  "displayName": "sp_dev101",
+  "passw0rd": "sp-pass",
+  "tenant": "tenant-id"
+}
+
+execute in your local sandbox bash terminal:
+az login --service-principal -u "sp-clientid" --tenant "tenant-id"
+az account set -s subs-id
+
+clone repo and execute terraform
+terraform init -upgrade
+terraform plan -out main.tfplan
+terraform apply main.tfplan
+```
---
+++ createAKS-tf/providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id   = "subs-id"
+  tenant_id         = "tenant-id"
+  client_id         = "sp-appid"
+  client_s3cr3t     = "sp-secret"
+}
